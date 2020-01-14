a4dfc8c2588d9c624f87852205e4dadd22835ebe
Suhendra <suhendra0812@gmail.com>
2020-01-14 15:40:02 +0800

    mengedit path pada created_tb.py

--- create_tb.py
+++ create_tb.py
@@ -1,6 +1,6 @@
 import os
 
-runlayout_path = "D:\\BARATA\\11.barata_layout\\create_tb.py"
+runlayout_path = "D:\\BARATA\\11.barata_layout\\barata_tb.py"
 
 os.system(f'python-qgis {runlayout_path}')
 
