3ffae1a814b0c408421237dd5a02d7537263d13e
Suhendra <suhendra0812@gmail.com>
2020-02-12 09:33:37 +0800

    menambahkan opsi layout ship dan oils

--- create_layout.py
+++ create_layout.py
@@ -2,12 +2,15 @@ import os
 
 option = input("\nPilih tipe project (ship/oils/dto): ")
 
+layoutship_path = "layout_ship.py"
+layoutoils_path = "layout_oils.py"
 layoutdto_path = "layout_dto.py"
-runlayout_path = "run_layout.py"
 
-if option == 'dto':
-    os.system(f'python-qgis {layoutdto_path}')
+if option == 'ship':
+    os.system(f'python-qgis {layoutship_path}')
+elif option == 'oils':
+    os.system(f'python-qgis {layoutoils_path}')
 else:
-    os.system(f'python-qgis {runlayout_path} {option}')
+    os.system(f'python-qgis {layoutdto_path}')
 
 input()
