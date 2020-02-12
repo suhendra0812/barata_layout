e736a54e2e267908f5361c717ab214f73c754b77
Suhendra <suhendra0812@gmail.com>
2020-02-12 09:36:39 +0800

    menyesuaikan format script

--- layout_dto.py
+++ layout_dto.py
@@ -1,10 +1,6 @@
-from datetime import datetime, timedelta
-import sip
-import geopandas as gpd
-import glob
+import sys, os
 from barata_layout import *
 from dto_info import DTOInfo
-import read_kml
 
 #set QGIS application path and initialize it
 qgs_path = 'C:\\OSGeo4W64\\apps\\qgis'
@@ -18,6 +14,7 @@ TEMPLATE_PATH = 'templates'
 
 #get project information
 project_path = f'{TEMPLATE_PATH}\\project\\layout_dto.qgz'
+
 project_layout = Project(project_path)
 project_layout.removeLayerPanel()
 project_layout.removeLayerHistory()
@@ -34,25 +31,37 @@ dto_path = FileDialog(BASE_PATH).open(type=project_type)
 print ('Sumber data:')
 print (dto_path)
 
+#get dto feature list
 dtofeat_list = DTOLayer(dto_path).getFeatList()
 
+OUTPUT_FOLDER = os.path.dirname(dto_path)
+
+#get dto info list
 dtoinfo_list = []
 for f in range(len(dtofeat_list)):
     idx = f + 1
     dtoinfo = DTOInfo(dto_path, idx)
     dtoinfo_list.append(dtoinfo)
 
+print(f'Jumlah DTO: {len(dtofeat_list)}')
+print(f'Radar: {dtoinfo_list[-1].sat}')
+
+#get variable from dto info
 wil = os.path.basename(dto_path)[:-4][:-4][:-16]
 local = dtoinfo_list[-1].local
 layer_name = f'{wil}_{local}_{project_type}'
-OUTPUT_FOLDER = os.path.dirname(dto_path)
 dtogdf_path = f'{OUTPUT_FOLDER}\\{layer_name}.shp'
 
+#create and get dto temporary layer
 templayer = DTOLayer(dto_path, dtoinfo_list).getLayer()
+
+#export the temporary layer to shapefile
 ExportLayer(templayer, dtogdf_path).to_shp()
 
+#get dto layer list from exported shapefile
 dtolayer_list = DataLayer([dtogdf_path]).getLayerList()
 
+#load dto layer to project
 dto_template = f'{TEMPLATE_PATH}\\layer\\dto_layer_template.qml'
 for dtolayer in dtolayer_list:
     dtolayer.setName(dtoinfo_list[-1].sat_id)
@@ -65,7 +74,10 @@ extent = LayerExtent([dtolayer]).getExtent()
 wpp_path = f'{BASEMAP_PATH}\\WPP_Full_PermenKP182014.shp'
 wpp_layer = WPPLayer(wpp_path, extent)
 
+#define layout manager
 layout_manager = LayoutDTO(project_type, dtoinfo_list, wpp_layer)
+
+#set and insert attributes to layout
 layout_manager.setMap(extent)
 layout_manager.setAtlas(dtolayer)
 layout_manager.insertTitleText()
@@ -74,6 +86,10 @@ layout_manager.insertNoteText()
 #save project
 outputproj_path = f'{OUTPUT_FOLDER}\\{layer_name}.qgz'
 project_layout.saveProject(outputproj_path)
+
+print ('\nLayout telah dibuat\n')
+
+print ('\nSelesai')
 
 qgs_app.quit()
 
