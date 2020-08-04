c7a04f0651b2d86c1886107054d0cba19120360d
Suhendra <suhendra0812@gmail.com>
2020-08-05 07:52:19 +0800

    mengganti keyword cmd 'python-qgis' dengan path lengkap dari 'python-qgis.bat'

--- create_layout.py
+++ create_layout.py
@@ -4,16 +4,18 @@ layoutship_path = "layouts/layout_ship.py"
 layoutoils_path = "layouts/layout_oils.py"
 layoutdto_path = "layouts/layout_dto.py"
 
+python_qgis = 'C:\\OSGeo4W64\\bin\\python-qgis.bat'
+
 while True:
     option = input("\nPilih tipe project (ship/oils/dto): ")
     if option.lower() == 'ship':
-        os.system(f'python-qgis {layoutship_path}')
+        os.system(f'{python_qgis} {layoutship_path}')
         break
     elif option.lower() == 'oils':
-        os.system(f'python-qgis {layoutoils_path}')
+        os.system(f'{python_qgis} {layoutoils_path}')
         break
     elif option.lower() == 'dto':
-        os.system(f'python-qgis {layoutdto_path}')
+        os.system(f'{python_qgis} {layoutdto_path}')
         break
     else:
         print('Tipe project yang Anda masukkan tidak sesuai')
